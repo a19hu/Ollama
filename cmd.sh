4977e928eded655bc9db358621b9d35cc831995f
a19hu <ashutoshkumar1jkl@gmail.com>
2025-03-19 23:21:39 +0530

    learn ollama

---
+++ cmd.sh
@@ -0,0 +1,19 @@
+ollama list
+ollama
+ollama rm <>
+ollama stop <>
+ollama help
+
+
+
+ollama run <>
+ollama pull <>
+
+
+
+>>> /show info
+>>> /?
+
+# create own ollama model
+
+ollama create <name>:<01> -f ./Modelfile
